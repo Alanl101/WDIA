52173d38cee2ce9700b99e3f1265a0b36e44edbe
Alanl101 <70230548+Alanl101@users.noreply.github.com>
2024-02-20 00:19:32 -0500

    aligning fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
     <main className="min-h-screen text-center sm:text-left">
       <div id="svg-img" className="xl:h-screen lg:h-full md:h-full sm:h-max min-h-screen flex justify-items-center">
 
-        <div className='grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 w-full mx-24 my-32 h-screen-64'>
+        <div className='grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 my-32 mx-6 h-screen-64'>
 
           <div className='flex w-full mt-12 pr-10 h-32 items-center justify-center'>
             <h1 className="text-5xl text-center text-secondary font-bold"> All Your Construction Needs In One Place </h1>
           </div>
 
-          <div>
-            <Image src={pic} alt="Company Logo" width={680} height={680} />
+          <div className="align-center">
+            <Image src={pic} alt="Company Logo" width={780} height={680} />
           </div>
           {/*<div className='flex w-full h-64 max-w-7xl justify-self-center items-center justify-center border-b-4 border-zinc-200'>
             <p className="text-xl text-secondary font-semibold text-justify mb-8 mt-32">
