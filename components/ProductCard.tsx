433c04222808cb7adcfa3142181599b965b7743a
Alanl101 <70230548+Alanl101@users.noreply.github.com>
2024-02-24 14:05:25 -0500

    Example Card fixed.

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -12,7 +12,9 @@ const ProductCard: React.FC<ProductCardProps> = ({ image, title, description }) 
   return (
     <div className="flex flex-col items-center justify-center mt-4 border border-grey-500  max-w-sm p-4 rounded mx-auto my-auto hover:shadow-lg transition duration-300">
       <h1 className="text-orange-600 text-xl font-bold ">{title}</h1>
-      <Image src={image} alt="User" width={200} height={200} unoptimized={true} />
+      <div className="mt-4"> 
+        <Image src={image} alt="Product" width={200} height={200} unoptimized={true} />
+      </div>
       <div className="font-semibold p-3 bg-gradient-to-b from-gray-100 to-white rounded mt-2">
         {description}
       </div>
